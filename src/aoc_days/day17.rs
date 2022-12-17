8629bc5ad7b2878ea2ac29f90f867b28e37cbe4d
a <a@b.com>
2022-12-17 17:35:04 +0000

    wip 17 refactor

--- src/aoc_days/day17.rs
+++ src/aoc_days/day17.rs
@@ -1,6 +1,5 @@
 use std::cmp::max;
 
-use grid::Grid;
 use inpt::{inpt, Inpt};
 use itertools::Itertools;
 
@@ -61,38 +60,62 @@ impl Shape {
         }
     }
 
-    fn cells(&self, x: usize, y: usize) -> Vec<(usize, usize)> {
-        let x = x as usize;
-        let y = y as usize;
+    fn cells(&self, x: usize) -> Vec<u8> {
         match self {
-            Shape::H => vec![(x, y), (x + 1, y), (x + 2, y), (x + 3, y)],
-            Shape::C => vec![
-                (x + 1, y),
-                (x, y + 1),
-                (x + 1, y + 1),
-                (x + 2, y + 1),
-                (x + 1, y + 2),
-            ],
-            Shape::L => vec![
-                (x, y),
-                (x + 1, y),
-                (x + 2, y),
-                (x + 2, y + 1),
-                (x + 2, y + 2),
-            ],
-            Shape::V => vec![(x, y), (x, y + 1), (x, y + 2), (x, y + 3)],
-            Shape::B => vec![(x, y), (x + 1, y), (x, y + 1), (x + 1, y + 1)],
+            Shape::H => vec![0b1111 << x],
+            Shape::C => vec![0b010 << x, 0b111 << x, 0b010 << x],
+            Shape::L => vec![0b111 << x, 0b100 << x, 0b100 << x],
+            Shape::V => vec![0b1 << x, 0b1 << x, 0b1 << x, 0b1 << x],
+            Shape::B => vec![0b11 << x, 0b11 << x],
         }
     }
 
-    fn stamp(&self, g: &mut Grid<bool>, x: usize, y: usize) {
-        for (x, y) in self.cells(x, y) {
-            g[y][x] = true;
-        }
+    fn stamp(&self, g: &mut Vec<u8>, x: usize, y: usize) {
+        let rows = g[y..(y + self.height())].iter_mut();
+        let cells = self.cells(x).into_iter();
+        rows.zip(cells).for_each(|(x, y)| *x |= y);
     }
 
-    fn is_blocked(&self, g: &Grid<bool>, x: usize, y: usize) -> bool {
-        self.cells(x, y).into_iter().any(|(x, y)| g[y][x])
+    fn is_blocked(&self, g: &Vec<u8>, x: usize, y: usize) -> bool {
+        let rows = g[y..(y + self.height())].into_iter();
+        let cells = self.cells(x).into_iter();
+        rows.zip(cells).any(|(x, y)| (x & y) != 0)
+    }
+
+    fn sim_drop<I: Iterator<Item = Direction>>(
+        self,
+        g: &mut Vec<u8>,
+        jets: &mut I,
+        mut x: usize,
+        mut y: usize,
+    ) -> usize {
+        while let Some(dir) = jets.next() {
+            let mut h = g.clone();
+            self.stamp(&mut h, x, y);
+            visualise(h);
+
+            match dir {
+                Direction::L => {
+                    if (x > 0) && !self.is_blocked(&g, x - 1, y) {
+                        x -= 1;
+                    }
+                }
+                Direction::R => {
+                    if ((x + 1 + self.width()) <= 7) && !self.is_blocked(&g, x + 1, y) {
+                        x += 1;
+                    }
+                }
+            }
+
+            if y == 0 || self.is_blocked(&g, x, y - 1) {
+                break;
+            }
+            y -= 1;
+        }
+
+        self.stamp(g, x, y);
+
+        y + self.height()
     }
 }
 
@@ -102,77 +125,35 @@ pub fn run(input: Input) -> () {
         .into_iter()
         .cycle();
 
-    let mut g: Grid<bool> = Grid::new(0, 7);
-    g.fill(true);
+    let mut g: Vec<u8> = Vec::new();
     let mut tallest: usize = 0;
 
-    for block in blocks.take(2022) {
+    for block in blocks.take(2) {
         // require at least tallest + shape.height + 3 rows
-        while g.rows() < tallest + block.height() + 3 {
-            g.push_row(vec![false, false, false, false, false, false, false]);
+        while g.len() < tallest + block.height() + 3 {
+            g.push(1 << 7);
         }
 
-        // let mut h = g.clone();
-        // block.stamp(&mut h, x, y);
-        // visualise(&h);
-
-        tallest = max(
-            tallest,
-            drop_block(&mut g, &mut jets, block, 2, tallest + 3),
-        );
+        tallest = max(tallest, block.sim_drop(&mut g, &mut jets, 2, tallest + 3));
     }
 
-    println!("{}", tallest);
+    println!("{tallest}",);
 
-    visualise(&g)
+    // visualise(g.clone());
 }
 
 // fn stamp_shape(g: Grid<bool>, (x, y): (usize, usize))
 
-fn visualise(g: &Grid<bool>) {
-    let mut rows = (0..g.rows())
-        .map(|row| {
-            g.iter_row(row)
-                .map(|&b| if b { '#' } else { '.' })
-                .collect::<String>()
-        })
-        .rev()
-        .collect_vec();
+fn visualise(mut g: Vec<u8>) {
+    g.reverse();
 
-    rows.push("-------".to_owned());
-
-    println!("{}", rows.join("\n"));
-    println!();
-}
-
-fn drop_block<I: Iterator<Item = Direction>>(
-    g: &mut Grid<bool>,
-    jets: &mut I,
-    block: Shape,
-    mut x: usize,
-    mut y: usize,
-) -> usize {
-    while let Some(dir) = jets.next() {
-        match dir {
-            Direction::L => {
-                if (x > 0) && !block.is_blocked(&g, x - 1, y) {
-                    x -= 1;
-                }
-            }
-            Direction::R => {
-                if ((x + 1 + block.width()) <= 7) && !block.is_blocked(&g, x + 1, y) {
-                    x += 1;
-                }
-            }
-        }
-
-        if y == 0 || block.is_blocked(&g, x, y - 1) {
-            break;
-        }
-        y -= 1;
-    }
-
-    block.stamp(g, x, y);
-
-    y + block.height()
+    println!(
+        "{}",
+        g.into_iter()
+            .map(|row| format!("{:>08b}", row.reverse_bits()))
+            .join("\n")
+            .replace('0', ".")
+            .replace('1', "#")
+    );
+    println!("-------\n");
 }
